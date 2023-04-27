e250329f2a6b4e305cbf95c104d2f6a5552a451d
mtanksl <20389632+mtanksl@users.noreply.github.com>
2023-04-27 13:21:36 -0300

    contruction kit fix

--- mtanksl.OpenTibia.Game/CommandHandlers/PlayerUseItem/ConstructionKitHandler.cs
+++ mtanksl.OpenTibia.Game/CommandHandlers/PlayerUseItem/ConstructionKitHandler.cs
@@ -11,58 +11,52 @@ namespace OpenTibia.Game.CommandHandlers
     {
         private Dictionary<ushort, ushort> constructionKits = new Dictionary<ushort, ushort>()
         {
-            { 3901, 1650 },
-            { 3902, 1658 },
-            { 3903, 1666 },
-            { 3904, 1670 },
-            { 3905, 3813 },
-            { 3906, 3817 },
-            { 3907, 2093 },
-            { 3908, 2603 },
-            { 3909, 1614 },
-            { 3910, 1615 },
-            { 3911, 1616 },
-            { 3912, 1619 },
-            { 3913, 3805 },
-            { 3914, 3807 },
-            { 3915, 1740 },
-            { 3916, 1774 },
-            { 3917, 2084 },
-            { 3918, 2095 },
-            { 3919, 3809 },
-            { 3920, 3832 },
-            { 3921, 1714 },
-            { 3922, 2107 },
-            { 3923, 2104 },
-            { 3924, 7670 },
-            { 3925, 1740 },
-            { 3926, 2080 },
-            { 3927, 2098 },
-            { 3928, 1676 },
-            { 3929, 2101 },
-            { 3930, 1739 },
-            { 3931, 2105 },
-            { 3932, 1724 },
-            { 3933, 1728 },
-            { 3934, 1732 },
-            { 3935, 1775 },
-            { 3936, 3812 },
-            { 3937, 2064 },
-            { 3938, 6371 },
-            { 5086, 1738 },
-            { 5087, 1741 },
-            { 5088, 1770 },
-            { 6114, 2106 },
-            { 6115, 2034 },
-            { 6372, 3811 },
-            { 6373, 1736 },
-            { 7503, 1750 },
-            { 7700, 5928 },
-            { 7960, 3821 },
-            { 7961, 3811 },
-            { 7962, 2582 },
-            { 8692, 8688 },
-            { 8693, 7486 }
+            { 3901, 1652 }, // Wooden chair
+            { 3902, 1658 }, // Sofa chair
+            { 3903, 1666 }, // Red cushioned chair
+            { 3904, 1670 }, // Green cushioned chair
+            { 3905, 3813 }, // Tusk chair
+            { 3906, 3817 }, // Ivony chair
+            { 3907, 3821 }, // Trunk chair
+            { 3908, 1619 }, // Small table
+            { 3909, 1614 }, // Big table
+            { 3910, 1615 }, // Square table
+            { 3911, 1616 }, // Round table
+            { 3912, 2603 }, // Coal basin
+            { 3913, 3805 }, // Stone table
+            { 3914, 3807 }, // Tusk table
+            { 3915, 1716 }, // Drawer
+            { 3916, 1724 }, // Dresser
+            { 3917, 1728 }, // Pendulum clock
+            { 3918, 1732 }, // Locker
+            { 3919, 3809 }, // Bamboo table
+            { 3920, 3811 }, // Trunk table
+            { 3921, 2084 }, // Harp
+            { 3922, 2094 }, // Birdcage
+            { 3923, 2098 }, // Globe
+            { 3924, 2064 }, // Table lamp
+            { 3925, 1674 }, // Rocking chair
+            { 3926, 2080 }, // Piano
+            { 3927, 1442 }, // Knight statue
+            { 3928, 1446 }, // Minotaur statue
+            { 3929, 2034 }, // Large amphora
+            { 3930, 1447 }, // Globin statue
+            { 3931, 2101 }, // Indoor plant
+            { 3932, 1770 }, // Barrel
+            { 3933, 2105 }, // Christmas tree
+            { 3934, 2117 }, // Rocking horse
+            { 3935, 2582 }, // Telescope
+            { 3936, 3832 }, // Bamboo drawer
+            { 3937, 1775 }, // Through
+            { 3938, 1750 }, // Trunk
+            { 5086, 5056 }, // Monkey statue speak
+            { 5087, 5055 }, // Monkey statue hear
+            { 5088, 5046 }, // Monkey statue see
+            { 6114, 5852 }, // Weapon rack
+            { 6115, 6111 }, // Armor rack
+            { 6372, 6357 }, // Oven
+            { 6373, 6371 }, // Bookcase
+            { 8692, 8688 }, // Chimney
         };
 
         public override Promise Handle(Func<Promise> next, PlayerUseItemCommand command)
