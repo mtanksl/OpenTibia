d74ef160e2a111e9bc260b853bb63182eb29cee4
mtanksl <20389632+mtanksl@users.noreply.github.com>
2023-05-02 15:12:53 -0300

    monster say command

---
+++ mtanksl.OpenTibia.Game/Commands/Outgoing/Monster/MonsterSayCommand.cs
@@ -0,0 +1,41 @@
+﻿using OpenTibia.Common.Objects;
+using OpenTibia.Common.Structures;
+using OpenTibia.Game.Events;
+using OpenTibia.Network.Packets.Outgoing;
+
+namespace OpenTibia.Game.Commands
+{
+    public class MonsterSayCommand : Command
+    {
+        public MonsterSayCommand(Monster monster, string message)
+        {
+            Monster = monster;
+
+            Message = message;
+        }
+
+        public Monster Monster { get; set; }
+
+        public string Message { get; set; }
+
+        public override Promise Execute()
+        {
+            foreach (var observer in Context.Server.Map.GetObservers(Monster.Tile.Position) )
+            {
+                if (observer.Tile.Position.CanHearSay(Monster.Tile.Position) )
+                {
+                    if (observer is Player player)
+                    {
+                        Context.AddPacket(player.Client.Connection, new ShowTextOutgoingPacket(0, Monster.Name, 0, TalkType.MonsterSay, Monster.Tile.Position, Message) );
+                    }
+
+                    Context.AddEvent(observer, new MonsterSayEventArgs(Monster, Message) );
+                }
+            }
+
+            Context.AddEvent(new MonsterSayEventArgs(Monster, Message) );
+
+            return Promise.Completed;
+        }
+    }
+}
---
+++ mtanksl.OpenTibia.Game/Events/Monster/MonsterSayEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using OpenTibia.Common.Objects;
+
+namespace OpenTibia.Game.Events
+{
+    public class MonsterSayEventArgs : GameEventArgs
+    {
+        public MonsterSayEventArgs(Monster monster, string message)
+        {
+            Monster = monster;
+
+            Message = message;
+        }
+
+        public Monster Monster { get; }
+
+        public string Message { get; }
+    }    
+}
