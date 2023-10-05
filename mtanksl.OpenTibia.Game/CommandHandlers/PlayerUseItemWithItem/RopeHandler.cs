0b141c2cf0741f08ceef1ea09b732e9999560a77
mtanksl <20389632+mtanksl@users.noreply.github.com>
2023-10-04 23:14:19 -0300

    More holes

--- mtanksl.OpenTibia.Game/CommandHandlers/PlayerUseItemWithItem/RopeHandler.cs
+++ mtanksl.OpenTibia.Game/CommandHandlers/PlayerUseItemWithItem/RopeHandler.cs
@@ -12,38 +12,44 @@ namespace OpenTibia.Game.CommandHandlers
 
         private HashSet<ushort> ropeSpots = new HashSet<ushort> { 384, 418 };
 
-        private HashSet<ushort> holes = new HashSet<ushort>() 
+        private HashSet<ushort> holes = new HashSet<ushort>()
         { 
             // Pitfall
             294,
 
             // Hole
-            383, 469, 470, 482, 484, 485, 369, 409, 410, 411, 
+            383, 469, 470, 482, 484, 485, 
 
             // Invisible
             459, 
 
             // Wooden
-            369, 409, 410, 411,
-
+            369, 370, 408, 409, 410, 411, 8276, 8277, 8278, 7279, 8280, 8271, 8282, 
+            
             // Wooden
-            423, 427, 428, 429,
-
+            423, 427, 428, 429, 8283, 8284, 8285, 8286, 
+                        
             // White marble
-            432, 433,
+            432, 433, 9606,
 
             // Dark wooden
             3135, 3136, 3137, 3138,
-
+                       
             // Stone
             3219, 3220,
 
             // Black marble
-            4834, 4835, 
+            4834, 4835, 8170,
 
             // Gray marble
             4836, 4837,
 
+            // Flat roof
+            924, 5081,
+
+            // Grass roof
+            6173, 6174,
+                      
             // Stone
             475, 476, 479, 480, 6127, 6128, 6129, 6130,
 
@@ -51,7 +57,10 @@ namespace OpenTibia.Game.CommandHandlers
             6917, 6918, 6919, 6920, 6921, 6922, 6923, 6924,
 
             // Earth
-            8559, 8560, 8561, 8562, 8563, 8564, 8565, 8566
+            8559, 8560, 8561, 8562, 8563, 8564, 8565, 8566,
+
+            // Corkscrew
+            9874, 984
         };
 
         public override Promise Handle(Func<Promise> next, PlayerUseItemWithItemCommand command)
