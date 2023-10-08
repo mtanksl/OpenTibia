44720840c3167b8b033f615f7cfa7db141c475e0
mtanksl <20389632+mtanksl@users.noreply.github.com>
2023-10-08 01:30:28 -0300

    areaa

--- mtanksl.OpenTibia.GameData/data/plugins/runes/great fireball rune.lua
+++ mtanksl.OpenTibia.GameData/data/plugins/runes/great fireball rune.lua
@@ -5,13 +5,13 @@ end
 
 function onuserune(player, target, tile, item)
 	local area = {
-												{-1, -3}, {0, -3}, {1, -3},
-							{-2, -2}, {-1, -2}, {0, -2}, {1, -2}, {2, -2},
+							{-1, -3}, {0, -3}, {1, -3},
+				  {-2, -2}, {-1, -2}, {0, -2}, {1, -2}, {2, -2},
 		{-3, -1}, {-2, -1}, {-1, -1}, {0, -1}, {1, -1}, {2, -1}, {3, -1},
 		{-3, 0},  {-2, 0},  {-1, 0},  {0, 0},  {1, 0},  {2, 0},  {3, 0},
 		{-3, 1},  {-2, 1},  {-1, 1},  {0, 1},  {1, 1},  {2, 1},  {3, 1},
-							{-2, 2},  {-1, 2},  {0, 2},  {1, 2},  {2, 2},
-												{-1, 3},  {0, 3},  {1, 3}
+				  {-2, 2},  {-1, 2},  {0, 2},  {1, 2},  {2, 2},
+							{-1, 3},  {0, 3},  {1, 3}
 	}
 	local min, max = formula.generic(player.Level, player.Skills.MagicLevel, 50, 15)
 	command.creatureattackarea(player, false, tile.Position, area, projectiletype.Fire, magiceffecttype.firearea, attack.simple(nil, nil, animatedtextcolor.orange, min, max), nil)
