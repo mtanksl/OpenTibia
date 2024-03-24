571d38d4c8fa4c97d7f1c7d579d3f2382bd164aa
mtanksl <20389632+mtanksl@users.noreply.github.com>
2024-03-24 15:42:53 -0300

    Moved ItemTrade

--- mtanksl.OpenTibia.Game/Commands/Incoming/GameWindow/TradeWith/ParseAcceptTradeCommand.cs
+++ mtanksl.OpenTibia.Game/Commands/Incoming/GameWindow/TradeWith/ParseAcceptTradeCommand.cs
@@ -1,5 +1,7 @@
 ﻿using OpenTibia.Common.Objects;
 using OpenTibia.Network.Packets.Outgoing;
+using System;
+using System.Collections.Generic;
 
 namespace OpenTibia.Game.Commands
 {
@@ -28,7 +30,7 @@ namespace OpenTibia.Game.Commands
 
                     Context.Server.Tradings.RemoveTrading(trading);
 
-                    return Context.AddCommand(new ItemTradeCommand(trading) );
+                    return Swap(trading);
                 }
             }
             else
@@ -47,12 +49,73 @@ namespace OpenTibia.Game.Commands
 
                         Context.Server.Tradings.RemoveTrading(trading);
 
-                        return Context.AddCommand(new ItemTradeCommand(trading) );
+                        return Swap(trading);
                     }
                 }
             }
 
             return Promise.Break;
+        }
+
+        private Promise Swap(Trading trading)
+        {
+            List<Promise> promises = new List<Promise>();
+
+            switch (trading.Offer.Parent)
+            {
+                case Tile fromTile:
+
+                    promises.Add(Context.AddCommand(new TileRemoveItemCommand(fromTile, trading.Offer) ) );
+
+                    break;
+
+                case Inventory fromInventory:
+
+                    promises.Add(Context.AddCommand(new InventoryRemoveItemCommand(fromInventory, trading.Offer) ) );
+
+                    break;
+
+                case Container fromContainer:
+
+                    promises.Add(Context.AddCommand(new ContainerRemoveItemCommand(fromContainer, trading.Offer) ) );
+
+                    break;
+
+                default:
+
+                    throw new NotImplementedException();
+            }
+
+            switch (trading.CounterOffer.Parent)
+            {
+                case Tile fromTile:
+
+                    promises.Add(Context.AddCommand(new TileRemoveItemCommand(fromTile, trading.CounterOffer) ) );
+
+                    break;
+
+                case Inventory fromInventory:
+
+                    promises.Add(Context.AddCommand(new InventoryRemoveItemCommand(fromInventory, trading.CounterOffer) ) );
+
+                    break;
+
+                case Container fromContainer:
+
+                    promises.Add(Context.AddCommand(new ContainerRemoveItemCommand(fromContainer, trading.CounterOffer) ) );
+
+                    break;
+
+                default:
+
+                    throw new NotImplementedException();
+            }
+
+            promises.Add(Context.AddCommand(new PlayerAddItemCommand(trading.OfferPlayer, trading.CounterOffer) ) );
+
+            promises.Add(Context.AddCommand(new PlayerAddItemCommand(trading.CounterOfferPlayer, trading.Offer) ) );
+
+            return Promise.WhenAll(promises.ToArray() );
         }
     }
 }
