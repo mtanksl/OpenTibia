451c9ae3265d3036b8c0398088fd03b53125bd28
mtanksl <20389632+mtanksl@users.noreply.github.com>
2025-01-08 00:20:59 -0300

    Desintegrate Rune

---
+++ mtanksl.OpenTibia.Plugins/Runes/DesintegrateRunePlugin.cs
@@ -0,0 +1,34 @@
+﻿using OpenTibia.Common.Objects;
+using OpenTibia.Common.Structures;
+using OpenTibia.Game.Commands;
+using OpenTibia.Game.Common;
+using OpenTibia.Game.Plugins;
+
+namespace OpenTibia.Plugins.Runes
+{
+    public class DesintegrateRunePlugin : RunePlugin
+    {
+        public DesintegrateRunePlugin(Rune rune) : base(rune)
+        {
+
+        }
+
+        public override PromiseResult<bool> OnUsingRune(Player player, Creature target, Tile tile, Item item)
+        {
+            if (tile == null || tile.Ground == null || tile.TopItem == null || tile.TopItem.Metadata.Flags.Is(ItemMetadataFlags.NotMoveable) || !player.Tile.Position.IsNextTo(tile.Position) )
+            {
+                return Promise.FromResultAsBooleanFalse;
+            }
+
+            return Promise.FromResultAsBooleanTrue;
+        }
+
+        public override Promise OnUseRune(Player player, Creature target, Tile tile, Item item)
+        {
+            return Context.AddCommand(new ShowMagicEffectCommand(tile.Position, MagicEffectType.Puff) ).Then( () =>
+            {
+                return Context.AddCommand(new ItemDestroyCommand(tile.TopItem) );
+            } );
+        }
+    }
+}
