957d1d37cfa16d94d70181f19091a261d4eec021
mtanksl <20389632+mtanksl@users.noreply.github.com>
2025-02-24 21:35:58 -0300

    GetImage all layers GetImage colored outfit

--- mtanksl.OpenTibia.FileFormats/Dat/Item.cs
+++ mtanksl.OpenTibia.FileFormats/Dat/Item.cs
@@ -3,7 +3,9 @@ using OpenTibia.IO;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace OpenTibia.FileFormats.Dat
 {
@@ -298,7 +300,7 @@ namespace OpenTibia.FileFormats.Dat
             
             Bitmap bitmap = new Bitmap(32 * Width, 32 * Height);
 
-            using ( Graphics graphics = Graphics.FromImage(bitmap) )
+            using (Graphics graphics = Graphics.FromImage(bitmap) )
             {
                 /*
                 int index = ZRepeat * YRepeat * XRepeat * Layers * Width * Height * animation +
@@ -322,7 +324,9 @@ namespace OpenTibia.FileFormats.Dat
 
                         if (spriteId > 0)
                         {
-                            graphics.DrawImage(sprites.First(sprite => sprite.Id == spriteId).GetImage(), 32 * i, 32 * j);
+                            Bitmap image = sprites.First(sprite => sprite.Id == spriteId).GetImage();
+
+                            graphics.DrawImage(image, 32 * i, 32 * j);
                         }
                     }
                 }
@@ -333,50 +337,116 @@ namespace OpenTibia.FileFormats.Dat
         
         public Bitmap GetImage(List<Sprite> sprites, int animation, int z, int y, int x)
         {
-            animation = Math.Min(Animations - 1, animation);
-
-            z = Math.Min(ZRepeat - 1, z);
-
-            y = Math.Min(YRepeat - 1, y);
-
-            x = Math.Min(XRepeat - 1, x);
-
             Bitmap bitmap = new Bitmap(32 * Width, 32 * Height);
 
-            using ( Graphics graphics = Graphics.FromImage(bitmap) )
+            using (Graphics graphics = Graphics.FromImage(bitmap) )
             {
-                /*
-                int index = ZRepeat * YRepeat * XRepeat * Layers * Width * Height * animation +
-
-                            YRepeat * XRepeat * Layers * Width * Height * z +
-
-                            XRepeat * Layers * Width * Height * y +
-
-                            Layers * Width * Height * x +
-
-                            Width * Height * layer;
-                */
-
                 for (int l = 0; l < Layers; l++)
                 {
-                    int index = Width * Height * (Layers * (XRepeat * (YRepeat * (ZRepeat * animation + z) + y) + x) + l);
+                    Bitmap image = GetImage(sprites, animation, z, y, x, l);
 
-                    for (int j = Height - 1; j >= 0; j--)
-                    {
-                        for (int i = Width - 1; i >= 0; i--)
-                        {
-                            ushort spriteId = spriteIds[index++];
-
-                            if (spriteId > 0)
-                            {
-                                graphics.DrawImage(sprites.First(sprite => sprite.Id == spriteId).GetImage(), 32 * i, 32 * j);
-                            }
-                        }
-                    }
+                    graphics.DrawImage(image, 0, 0);
                 }
             }
 
             return bitmap;
         }  
+
+        public Bitmap GetImage(List<Sprite> sprites, int animation, int z, int y, int x, Color replaceYellow, Color replaceRed, Color replaceGreen, Color replaceBlue)
+        {
+            if (Layers == 2)
+            {
+                Bitmap bitmap = new Bitmap(32 * Width, 32 * Height);
+
+                using (Graphics graphics = Graphics.FromImage(bitmap) )
+                {
+                    Bitmap imageMask = GetImage(sprites, animation, z, y, x, 1);
+
+                        ReplaceColor(imageMask, Color.FromArgb(255, 255, 0), replaceYellow);
+
+                        ReplaceColor(imageMask, Color.FromArgb(255, 0, 0), replaceRed);
+
+                        ReplaceColor(imageMask, Color.FromArgb(0, 255, 0), replaceGreen);
+
+                        ReplaceColor(imageMask, Color.FromArgb(0, 0, 255), replaceBlue);
+
+                    Bitmap imageBase = GetImage(sprites, animation, z, y, x, 0);
+
+                        MultiplicativeBlending(imageBase, imageMask);
+
+                    graphics.DrawImage(imageBase, 0, 0);
+                }
+
+                return bitmap;
+            }
+                
+            return GetImage(sprites, animation, z, y, x);           
+        }   
+
+        private void ReplaceColor(Bitmap bitmap, Color oldColor, Color newColor)
+        {
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            byte[] pixels = new byte[bitmapData.Stride /* = 4 * bitmap.Width */ * bitmap.Height];
+
+            Marshal.Copy(bitmapData.Scan0, pixels, 0, pixels.Length);
+
+            for (int i = 0; i < pixels.Length; i += 4)
+            {
+                // 0 1 2 3
+                // B G R A
+
+                if (pixels[i + 2] == oldColor.R && 
+                    pixels[i + 1] == oldColor.G &&
+                    pixels[i + 0] == oldColor.B)
+                {
+                    pixels[i + 2] = newColor.R;
+                    pixels[i + 1] = newColor.G;
+                    pixels[i + 0] = newColor.B;
+                }
+            }
+
+            Marshal.Copy(pixels, 0, bitmapData.Scan0, pixels.Length);
+
+            bitmap.UnlockBits(bitmapData);
+        }
+
+        private void MultiplicativeBlending(Bitmap bitmap, Bitmap mask)
+        {
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            byte[] pixels = new byte[bitmapData.Stride /* = 4 * bitmap.Width */ * bitmap.Height];
+
+            Marshal.Copy(bitmapData.Scan0, pixels, 0, pixels.Length);
+
+
+            BitmapData bitmapData2 = mask.LockBits(new Rectangle(0, 0, mask.Width, mask.Height), ImageLockMode.ReadOnly, mask.PixelFormat);
+
+            byte[] pixels2 = new byte[bitmapData2.Stride /* = 4 * mask.Width */ * mask.Height];
+
+            Marshal.Copy(bitmapData2.Scan0, pixels2, 0, pixels2.Length);
+
+            mask.UnlockBits(bitmapData2);
+
+
+            for (int i = 0; i < pixels.Length; i += 4)
+            {
+                // 0 1 2 3
+                // B G R A
+
+                if (pixels2[i + 2] != 0 || 
+                    pixels2[i + 1] != 0 || 
+                    pixels2[i + 0] != 0)
+                {
+                    pixels[i + 2] = (byte)(pixels[i + 2] * pixels2[i + 2] / 255);
+                    pixels[i + 1] = (byte)(pixels[i + 1] * pixels2[i + 1] / 255);
+                    pixels[i + 0] = (byte)(pixels[i + 0] * pixels2[i + 0] / 255);
+                }
+            }
+
+            Marshal.Copy(pixels, 0, bitmapData.Scan0, pixels.Length);
+
+            bitmap.UnlockBits(bitmapData);
+        }
     }
 }
